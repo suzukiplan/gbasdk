3928db5f23906b4548322b2bb08bdc17d7d78707
suzukiplan <suzukiplan.ys@gmail.com>
2025-03-18 09:12:52 +0900

    comment in

--- src/psg.h
+++ src/psg.h
@@ -7,6 +7,10 @@ extern "C" {
 void psg_init(void);
 void psg_play(const void* data);
 void psg_stop(void);
+
+// This function is called every frame when VBLANK is reached,
+// but the user program does not need to call it
+// because it is automatically called from `vdp_wait_vblank`.
 void psg_frame(void);
 
 #ifdef __cplusplus
